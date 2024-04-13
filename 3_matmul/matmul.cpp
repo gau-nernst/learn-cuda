ce00befb64b3897f741210714186c9f0f769e2d9
Thien Tran <gau.nernst@yahoo.com.sg>
2024-04-13 13:29:08 +0800

    use template to generate PyTorch wrapper

--- 3_matmul/matmul.cpp
+++ 3_matmul/matmul.cpp
@@ -6,11 +6,13 @@
   CHECK_CUDA(x);                                                                                                       \
   CHECK_CONTIGUOUS(x)
 
-void matmul_v1(const float *A, const float *B, float *C, int M, int N, int K);
-void matmul_v2(const float *A, const float *B, float *C, int M, int N, int K);
-void matmul_v3(const float *A, const float *B, float *C, int M, int N, int K);
+typedef void MatmulFn(const float *A, const float *B, float *C, int M, int N, int K);
 
-torch::Tensor matmul_v1_pt(torch::Tensor A, torch::Tensor B) {
+MatmulFn matmul_v1;
+MatmulFn matmul_v2;
+MatmulFn matmul_v3;
+
+template <MatmulFn matmul_fn> torch::Tensor matmul_pt(torch::Tensor A, torch::Tensor B) {
   CHECK_INPUT(A);
   CHECK_INPUT(B);
   TORCH_CHECK(A.size(1) == B.size(0), "dim1 of input2 should be equal to dim2 of input1");
@@ -18,36 +20,12 @@ torch::Tensor matmul_v1_pt(torch::Tensor A, torch::Tensor B) {
   int K = A.size(1);
   int N = B.size(1);
   torch::Tensor C = torch::empty({M, N}, A.options());
-  matmul_v1(A.data_ptr<float>(), B.data_ptr<float>(), C.data_ptr<float>(), M, N, K);
-  return C;
-}
-
-torch::Tensor matmul_v2_pt(torch::Tensor A, torch::Tensor B) {
-  CHECK_INPUT(A);
-  CHECK_INPUT(B);
-  TORCH_CHECK(A.size(1) == B.size(0), "dim1 of input2 should be equal to dim2 of input1");
-  int M = A.size(0);
-  int K = A.size(1);
-  int N = B.size(1);
-  torch::Tensor C = torch::empty({M, N}, A.options());
-  matmul_v2(A.data_ptr<float>(), B.data_ptr<float>(), C.data_ptr<float>(), M, N, K);
-  return C;
-}
-
-torch::Tensor matmul_v3_pt(torch::Tensor A, torch::Tensor B) {
-  CHECK_INPUT(A);
-  CHECK_INPUT(B);
-  TORCH_CHECK(A.size(1) == B.size(0), "dim1 of input2 should be equal to dim2 of input1");
-  int M = A.size(0);
-  int K = A.size(1);
-  int N = B.size(1);
-  torch::Tensor C = torch::empty({M, N}, A.options());
-  matmul_v3(A.data_ptr<float>(), B.data_ptr<float>(), C.data_ptr<float>(), M, N, K);
+  matmul_fn(A.data_ptr<float>(), B.data_ptr<float>(), C.data_ptr<float>(), M, N, K);
   return C;
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("matmul_v1", &matmul_v1_pt, "Matrix multiplication v1");
-  m.def("matmul_v2", &matmul_v2_pt, "Matrix multiplication v2");
-  m.def("matmul_v3", &matmul_v3_pt, "Matrix multiplication v2");
+  m.def("matmul_v1", &matmul_pt<matmul_v1>, "Matrix multiplication v1");
+  m.def("matmul_v2", &matmul_pt<matmul_v2>, "Matrix multiplication v2");
+  m.def("matmul_v3", &matmul_pt<matmul_v3>, "Matrix multiplication v2");
 }
