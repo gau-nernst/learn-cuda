084873bec7cc1ca5f5b787d46570cd1b54e2527a
Thien Tran <gau.nernst@yahoo.com.sg>
2024-04-13 08:49:33 +0800

    add triton reference implementation

--- 3_matmul/main.py
+++ 3_matmul/main.py
@@ -13,17 +13,19 @@ module = torch.utils.cpp_extension.load(
 )
 
 # for large n, there will be a larger deviation, since sum of many small elements are not accurate
-input1 = torch.randn(1000, 200, device="cuda")
-input2 = torch.randn(200, 1000, device="cuda")
+input1 = torch.randn(4096, 4096, device="cuda")
+input2 = torch.randn(4096, 4096, device="cuda")
 
 output_v1 = module.matmul_v1(input1, input2)
 output_v2 = module.matmul_v2(input1, input2)
 output_triton = matmul_triton.matmul(input1, input2)
+output_triton_ref = matmul_triton.matmul_ref(input1, input2)
 
 output_ref = torch.matmul(input1, input2)
 torch.testing.assert_close(output_v1, output_ref)
 torch.testing.assert_close(output_v2, output_ref)
 torch.testing.assert_close(output_triton, output_ref)
+torch.testing.assert_close(output_triton_ref, output_ref)
 
 
 def benchmark(fn, *args):
@@ -42,3 +44,4 @@ benchmark(torch.matmul, input1, input2)
 benchmark(module.matmul_v1, input1, input2)
 benchmark(module.matmul_v2, input1, input2)
 benchmark(matmul_triton.matmul, input1, input2)
+benchmark(matmul_triton.matmul_ref, input1, input2)
