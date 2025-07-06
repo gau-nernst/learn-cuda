cf6f1a0463dcc20360e6275eb97f2ef7d361b82a
Thien Tran <gau.nernst@yahoo.com.sg>
2025-07-06 12:14:11 +0800

    add flash-attn as baseline

--- 8_attention/README.md
+++ 8_attention/README.md
@@ -8,10 +8,11 @@ For bs=1, num_heads=8, len_query=4096, len_kv = 8192. 5090 @ 400W, compile with 
 
 Kernel                         | TFLOPS | % of SOL
 -------------------------------|--------|---------
-`F.sdpa()` (Flash Attention)   | 185.19 | 88.40%
-`F.sdpa()` (CuDNN)             | 202.18 | 96.51%
-v1                             | 141.13 | 67.36%
-v2 (shared memory swizzling)   | 179.62 | 85.74%
-v3 (2-stage pipelining)        | 186.93 | 89.23%
-v4 (`ldmatrix.x4` for K and V) | 192.01 | 91.65%
-v5 (better pipelining)         | 196.44 | 93.77%
+`F.sdpa()` (Flash Attention)   | 186.73 | 89.13%
+`F.sdpa()` (CuDNN)             | 203.61 | 97.19%
+`flash-attn`                   | 190.58 | 90.97%
+v1                             | 142.87 | 68.20%
+v2 (shared memory swizzling)   | 181.11 | 86.45%
+v3 (2-stage pipelining)        | 189.84 | 90.62%
+v4 (`ldmatrix.x4` for K and V) | 194.33 | 92.76%
+v5 (better pipelining)         | 197.74 | 94.39%
--- 8_attention/main.py
+++ 8_attention/main.py
@@ -2,11 +2,17 @@ import argparse
 import time
 from pathlib import Path
 
+import pandas as pd
 import torch
 import torch.nn.functional as F
 from torch.nn.attention import SDPBackend, sdpa_kernel
 from torch.utils.cpp_extension import load
 from triton.testing import do_bench
+
+try:
+    from flash_attn import flash_attn_func
+except ImportError:
+    flash_attn_func = None
 
 CURRENT_DIR = Path(__file__).parent
 
@@ -64,27 +70,47 @@ def main():
     }
     sol = SOL_LOOKUP.get(torch.cuda.get_device_name(), 0)
 
-    def bench_and_print(f, name):
+    results = []
+
+    def bench_and_print(f, name, *args):
         # sleep to stabilize thermal
         time.sleep(1)
 
-        latency_ms = do_bench(lambda: f(Q, K, V), return_mode="median")
+        latency_ms = do_bench(lambda: f(*args), return_mode="median")
         tflops = 4 * bs * nh * lq * lkv * head_dim / latency_ms / 1e9
         pct_sol = tflops / sol * 100
-        print(f"{name}:\t{latency_ms:.4f} ms\t{tflops:.2f} TFLOPS\t{pct_sol:.2f}% SOL")
+        results.append(
+            [name, round(latency_ms, 4), round(tflops, 2), round(pct_sol, 2)]
+        )
 
     out_ref = F.scaled_dot_product_attention(Q, K, V)
 
     with sdpa_kernel([SDPBackend.FLASH_ATTENTION]):
-        bench_and_print(F.scaled_dot_product_attention, "Flash")
+        bench_and_print(F.scaled_dot_product_attention, "F.sdpa() - FA", Q, K, V)
     with sdpa_kernel([SDPBackend.CUDNN_ATTENTION]):
-        bench_and_print(F.scaled_dot_product_attention, "CuDNN")
+        bench_and_print(F.scaled_dot_product_attention, "F.sdpa() - CuDNN", Q, K, V)
+
+    if flash_attn_func is not None:
+        out = flash_attn_func(
+            Q.transpose(1, 2), K.transpose(1, 2), V.transpose(1, 2)
+        ).transpose(1, 2)
+        torch.testing.assert_close(out, out_ref)
+        bench_and_print(
+            flash_attn_func,
+            "flash-attn",
+            Q.transpose(1, 2),
+            K.transpose(1, 2),
+            V.transpose(1, 2),
+        )
 
     for i in range(5):
         f = getattr(module, f"sdpa_v{i + 1}")
         out = f(Q, K, V)
         torch.testing.assert_close(out, out_ref)
-        bench_and_print(f, f"v{i + 1}")
+        bench_and_print(f, f"v{i + 1}", Q, K, V)
+
+    df = pd.DataFrame(results, columns=["Kernel", "Latency (ms)", "TFLOPS", "% SOL"])
+    print(df.to_markdown(index=False))
 
 
 if __name__ == "__main__":
