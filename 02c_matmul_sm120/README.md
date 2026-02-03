78fb398eb3cf6689939667184ac74599f16d032b
Thien Tran <gau.nernst@yahoo.com.sg>
2026-02-03 08:54:18 +0800

    add sweep

--- 02c_matmul_sm120/README.md
+++ 02c_matmul_sm120/README.md
@@ -3,15 +3,26 @@ Resources:
 - https://cudaforfun.substack.com/p/outperforming-cublas-on-h100-a-worklog - TMA
 - https://docs.nvidia.com/cuda/cuda-c-programming-guide/#asynchronous-data-copies-using-the-tensor-memory-accelerator-tma
 
-For M = N = K = 4096, BF16 A row-major x B column-major, compile with CUDA 13.0.
+BF16 A row-major x B column-major, compile with CUDA 13.0.
 - Theoretical limit: 209.5 TFLOPS for 5090, 503.8 for PRO 6000.
 
-Kernel name                    | 5090 @ 400W                | PRO 6000 @ 600W
--------------------------------|----------------------------|---------------------------
-CuBLAS 13.0 (via PyTorch 2.10) | 160.54 TFLOPS (76.63% SOL) | 353.23 TFLOPS (70.11% SOL)
-Inductor Triton (PyTorch 2.10) | 173.04 TFLOPS (82.60% SOL) | 331.99 TFLOPS (65.90% SOL)
-v0 (`cp.async`)                | 175.30 TFLOPS (83.68% SOL) | 329.52 TFLOPS (65.41% SOL)
-v1 (TMA)                       | 180.40 TFLOPS (86.11% SOL) | 360.80 TFLOPS (71.62% SOL)
+**5090**: Max 209.5 TFLOPS. Report `TFLOPS (%SOL)`
+
+Kernel name                    | 1024            | 2048            | 4096            | 8192
+-------------------------------|-----------------|-----------------|-----------------|----------------
+CuBLAS 13.0 (via PyTorch 2.10) |  81.44 (38.87%) | 140.14 (66.89%) | 160.54 (76.63%) | 202.88 (96.84%)
+Inductor Triton (PyTorch 2.10) | 104.86 (50.05%) | 139.41 (66.54%) | 173.04 (82.60%) | 201.49 (96.18%)
+v0 (`cp.async`)                | 103.09 (49.21%) | 133.15 (63.56%) | 175.30 (83.68%) | 167.22 (79.82%)
+v1 (TMA)                       | 104.86 (50.05%) | 137.52 (65.64%) | 180.40 (86.11%) | 172.66 (82.42%)
+
+**PRO 6000**: Max 503.8 TFLOPS. Report `TFLOPS (%SOL)`
+
+Kernel name                    | 1024            | 2048            | 4096            | 8192
+-------------------------------|-----------------|-----------------|-----------------|----------------
+CuBLAS 13.0 (via PyTorch 2.10) |  76.61 (15.21%) | 199.36 (39.57%) | 353.23 (70.11%) | 431.72 (85.69%)
+Inductor Triton (PyTorch 2.10) |  87.50 (17.37%) | 232.31 (46.11%) | 331.99 (65.90%) | 405.64 (80.52%)
+v0 (`cp.async`)                |  68.41 (13.58%) | 215.09 (42.69%) | 329.52 (65.41%) | 400.20 (79.44%)
+v1 (TMA)                       |  69.91 (13.88%) | 220.75 (43.82%) | 360.80 (71.62%) | 420.80 (83.53%)
 
 TODO:
 - Warp specialization
@@ -22,7 +33,7 @@ Learnings
   - `mbarrier` object is used to synchronize TMA transfer.
   - `fence.proxy.async.shared::cta` is required to make changes by CUDA thread visible to **async proxy** (TMA hardware?).
   - **Only 1 thread** is required to issue instructions to TMA.
-  - Use `cp_async_bulk_tensor_2d_global_to_shared()` / `cp.async.bulk.tensor.2d.shared::cluster.global.tile.mbarrier::complete_tx::bytes` to initiate TMA load. We can specify global memory offset to select the tile from global memory to load to shared memory.
+  - Use `cp.async.bulk.tensor.2d.shared::cluster.global.tile.mbarrier::complete_tx::bytes` to initiate TMA load. We can specify global memory offset to select the tile from global memory to load to shared memory.
   - Unit of data is 16-byte (1 row of `ldmatrix` / 4 memory banks, same as `cp.async.cg`). Swizzle layout will permute these data units.
   - `CU_TENSOR_MAP_SWIZZLE_128B`: 128-byte block `uint128_t[8][8]`. Column index is XOR-ed with row index. Note that this row/column does not correspond to shared memory row/column. Inner-most dim of shared memory must be <= 128 bytes. Looking at memory address, it XORs bit4-6 with bit7-9.
   - `CU_TENSOR_MAP_SWIZZLE_64B`: 64-byte block `uint128_t[4][8]`. Inner-most dim of shared memory must be <= 64 bytes. XOR bit4-5 with bit 7-8.
--- 02c_matmul_sm120/main.py
+++ 02c_matmul_sm120/main.py
@@ -21,24 +21,34 @@ module = torch.utils.cpp_extension.load(
     verbose=True,
 )
 
+SOL_LOOKUP = {
+    "NVIDIA GeForce RTX 5090": 209.5,
+    "NVIDIA RTX PRO 6000 Blackwell Server Edition": 503.8,
+}
+SOL = SOL_LOOKUP.get(torch.cuda.get_device_name(), 1e9)
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--profile")
-    parser.add_argument("--shape", type=int, nargs="+", default=[4096, 4096, 4096])
-    args = parser.parse_args()
 
+def bench_and_print(f, name: str, M: int, N: int, K: int):
+    # sleep to stabilize thermal
+    time.sleep(1)
+
+    latency_ms = do_bench(f, return_mode="median")
+    tflops = 2 * M * N * K / latency_ms / 1e9
+    pct_sol = tflops / SOL * 100
+    print(f"{name}:\t{latency_ms:.4f} ms\t{tflops:.2f} TFLOPS\t{pct_sol:.2f}% SOL")
+
+
+def main(args: argparse.Namespace):
     torch._inductor.config.max_autotune_gemm_backends = "TRITON"
     # torch._inductor.utils.is_big_gpu = lambda _: True
-
-    # for large K, there will be a larger deviation, since sum of many small elements are not accurate
-    M, N, K = args.shape
-    print(f"{M=}, {N=}, {K=}")
-    A = torch.randn(M, K).bfloat16().cuda()
-    B = torch.randn(N, K).bfloat16().cuda().T
     inductor_mm = torch.compile(torch.mm, mode="max-autotune-no-cudagraphs", dynamic=False)
 
     if args.profile is not None:
+        M, N, K = args.shape
+        print(f"{M=}, {N=}, {K=}")
+        A = torch.randn(M, K, device="cuda").div(1024).bfloat16().cuda()
+        B = torch.randn(N, K, device="cuda").div(1024).bfloat16().cuda().T
+
         if args.profile == "cublas":
             fn = torch.mm
         elif args.profile == "inductor":
@@ -49,30 +59,37 @@ def main():
         torch.cuda.synchronize()
         return
 
-    SOL_LOOKUP = {
-        "NVIDIA GeForce RTX 5090": 209.5,
-    }
-    sol = SOL_LOOKUP.get(torch.cuda.get_device_name(), 0)
+    if args.sweep is not None:
+        shapes = [(s, s, s) for s in args.sweep]
+    else:
+        assert len(args.shape) == 3
+        shapes = [args.shape]
 
-    def bench_and_print(f, name):
-        # sleep to stabilize thermal
-        time.sleep(1)
+    for M, N, K in shapes:
+        print(f"{M=}, {N=}, {K=}")
+        A = torch.randn(M, K, device="cuda").div(1024).bfloat16().cuda()
+        B = torch.randn(N, K, device="cuda").div(1024).bfloat16().cuda().T
 
-        latency_ms = do_bench(lambda: f(A, B), return_mode="median")
-        tflops = 2 * M * N * K / latency_ms / 1e9
-        pct_sol = tflops / sol * 100
-        print(f"{name}:\t{latency_ms:.4f} ms\t{tflops:.2f} TFLOPS\t{pct_sol:.2f}% SOL")
+        # reference in FP32 to avoid things like split-K
+        output_ref = torch.matmul(A.float(), B.float()).bfloat16()
 
-    output_ref = torch.matmul(A, B)
-    bench_and_print(torch.matmul, "CuBLAS")
-    bench_and_print(inductor_mm, "Inductor Triton")
+        bench_and_print(lambda: torch.matmul(A, B), "CuBLAS", M, N, K)
+        bench_and_print(lambda: inductor_mm(A, B), "Inductor Triton", M, N, K)
 
-    for i in range(3):
-        fn = getattr(module, f"matmul_v{i}")
-        output = fn(A, B)
-        torch.testing.assert_close(output, output_ref)
-        bench_and_print(fn, f"v{i}")
+        for i in range(3):
+            fn = getattr(module, f"matmul_v{i}")
+            output = fn(A, B)
+            torch.testing.assert_close(output, output_ref)
+            bench_and_print(lambda: fn(A, B), f"v{i}", M, N, K)
+
+        print()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--profile")
+    parser.add_argument("--shape", type=int, nargs="+", default=[4096, 4096, 4096])
+    parser.add_argument("--sweep", type=int, nargs="+")
+    args = parser.parse_args()
+
+    main(args)
